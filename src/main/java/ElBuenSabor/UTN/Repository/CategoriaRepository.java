b1f1057cdb8e113404fe7660ad5813091acf8e72
MauroPuebla02 <mauronico02@gmail.com>
2025-05-28 22:02:18 -0300

    query findByManufacturado and database

--- src/main/java/ElBuenSabor/UTN/Repository/CategoriaRepository.java
+++ src/main/java/ElBuenSabor/UTN/Repository/CategoriaRepository.java
@@ -10,11 +10,11 @@ public interface CategoriaRepository extends BaseRepository<Categoria, Long>{
 
     @Query(value= """
     SELECT C.ID AS ID, C.DENOMINACION AS DENOMINACION FROM CATEGORIA AS C JOIN CATEGORIA_ARTICULO AS CA
-        ON CA.CATEGORIA_ID = C.ID
-        JOIN ARTICULO_MANUFACTURADO AS AM
-        ON AM.ID = CA.ARTICULO_ID
-        JOIN ARTICULO AS A
-        ON A.ID = AM.ID WHERE A.ELIMINADO != TRUE AND C.ELIMINADO != TRUE
+            ON CA.CATEGORIA_ID = C.ID
+            JOIN ARTICULO_MANUFACTURADO AS AM
+            ON AM.ID = CA.ARTICULO_ID
+            JOIN ARTICULO AS A
+            ON A.ID = AM.ID WHERE A.ELIMINADO != TRUE AND C.ELIMINADO != TRUE GROUP BY C.ID
 """, nativeQuery = true)
     List<Categoria> findByManufacturado();
 }
