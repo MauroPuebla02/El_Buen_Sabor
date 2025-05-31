27c6e8dc0affa50c6756e8bf89a0863162f0e5bf
Ailen Bossio <ailenbossio123@gmail.com>
2025-05-30 23:03:09 -0300

    agrego relacion categoria y articulos

--- src/main/java/ElBuenSabor/UTN/Repository/ArticuloManufacturadoRepository.java
+++ src/main/java/ElBuenSabor/UTN/Repository/ArticuloManufacturadoRepository.java
@@ -14,18 +14,18 @@ public interface ArticuloManufacturadoRepository extends BaseRepository<Articulo
     @Query(value = """
 
             SELECT A.ID,
-                  A.DENOMINACION,
-                  AM.DESCRIPCION,
-                  A.PRECIO_VENTA,
-                  AM.TIEMPO_ESTIMADO_EN_MINUTOS
-           FROM CATEGORIA AS C
-           JOIN ARTICULO AS A
-           ON A.CATEGORIA_ID = C.ID
-           JOIN ARTICULO_MANUFACTURADO AS AM
-           ON AM.ID = A.ID
-           WHERE C.ID=?1 AND C.ELIMINADO = FALSE AND A.ELIMINADO = FALSE
+                       A.DENOMINACION,
+                       AM.DESCRIPCION,
+                       A.PRECIO_VENTA,
+                       AM.TIEMPO_ESTIMADO_EN_MINUTOS
+                FROM ARTICULO AS A
+                JOIN ARTICULO_MANUFACTURADO AS AM ON AM.ID = A.ID
+                WHERE A.CATEGORIA_ID = ?1
+                  AND A.ELIMINADO = FALSE
         """,
             nativeQuery = true)
     List<ArticuloManufacturadoByCategoriaDTO> findArticulosManufacturadosByCategoria(
             @Param("idCategoria") Long idCategoria);
+
+
 }
