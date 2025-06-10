37eb19ed7dc1486139e06da6db8834d19d425c14
FedeDAnna <danna.federico99@gmail.com>
2025-06-09 21:35:42 -0300

    algo

--- src/main/java/ElBuenSabor/UTN/Service/Interface/iArticuloInsumoService.java
+++ src/main/java/ElBuenSabor/UTN/Service/Interface/iArticuloInsumoService.java
@@ -1,11 +1,13 @@
 package ElBuenSabor.UTN.Service.Interface;
 
 import ElBuenSabor.UTN.Models.Model.ArticuloInsumo;
+import ElBuenSabor.UTN.Models.Model.ArticuloManufacturado;
 import ElBuenSabor.UTN.Service.BaseService;
 
 import java.util.List;
 
 public interface iArticuloInsumoService extends BaseService<ArticuloInsumo, Long> {
 
-    //List<ArticuloInsumo> findArticulosInsumosByTipoCategoria(Long idCategoria);
+    List<ArticuloInsumo> findArticulosInsumosByCategoria(Long idCategoria);
+
 }
