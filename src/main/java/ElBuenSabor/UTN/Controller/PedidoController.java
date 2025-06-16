820a78e300e2515e69374ba5ce0d1eff12375b0e
Ailen Bossio <ailenbossio123@gmail.com>
2025-06-15 22:07:30 -0300

    agrego patch y get de pedido

--- src/main/java/ElBuenSabor/UTN/Controller/PedidoController.java
+++ src/main/java/ElBuenSabor/UTN/Controller/PedidoController.java
@@ -1,12 +1,29 @@
 package ElBuenSabor.UTN.Controller;
 
+import ElBuenSabor.UTN.Models.DTO.ArticuloManufacturadoByCategoriaDTO;
+import ElBuenSabor.UTN.Models.DTO.ArticuloManufacturadoPedidoDTO;
+import ElBuenSabor.UTN.Models.DTO.EstadoUpdateRequestDTO;
 import ElBuenSabor.UTN.Models.Model.Pedido;
 import ElBuenSabor.UTN.Service.Implements.PedidoServiceImpl;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("pedidos")
+@RequestMapping("/pedidos")
 public class PedidoController extends BaseControllerImpl<Pedido, PedidoServiceImpl>{
     public PedidoController(PedidoServiceImpl service) {super(service);}
+
+    @GetMapping(value = "/manufacturados/{pedidoId}")
+    public List<ArticuloManufacturadoPedidoDTO> findAllArticuloManufacturadoByPedido(@PathVariable("pedidoId") Long pedidoId){
+        return  service.findAllArticuloManufacturadoByPedido(pedidoId);
+    }
+
+    @PatchMapping("/pedido/{pedidoId}")
+    public ResponseEntity<?> actualizarEstado(@PathVariable("pedidoId") Long id,
+                                                   @RequestBody EstadoUpdateRequestDTO dto) {
+        service.actualizarEstado(id, dto.getEstadoPedido());
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/ElBuenSabor/UTN/Repository/PedidoRepository.java
+++ src/main/java/ElBuenSabor/UTN/Repository/PedidoRepository.java
@@ -1,9 +1,35 @@
 package ElBuenSabor.UTN.Repository;
 
+import ElBuenSabor.UTN.Models.DTO.ArticuloManufacturadoPedidoDTO;
+import ElBuenSabor.UTN.Models.Model.Estado;
 import ElBuenSabor.UTN.Models.Model.Pedido;
 import ElBuenSabor.UTN.Models.Model.TipoEnvio;
 import ElBuenSabor.UTN.Models.Model.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PedidoRepository extends BaseRepository<Pedido,Long> {
+    @Query("""
+    SELECT 
+        am.id AS id,
+        am.denominacion AS denominacion,
+        am.precio_venta AS precioVenta,
+        pd.cantidad AS cantidad,
+        pd.subtotal AS subtotal
+    FROM Pedido p
+    JOIN p.detalles pd
+    JOIN TREAT(pd.articulo AS ArticuloManufacturado) am
+    WHERE p.id = :pedidoId
+""")
+    List<ArticuloManufacturadoPedidoDTO> findAllArticuloManufacturadoByPedido(@Param("pedidoId") Long pedidoId);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Pedido p SET p.estado_pedido = :nuevoEstado " +
+            "WHERE p.id = :id AND p.eliminado = false")
+    void actualizarEstado(Long id, Estado nuevoEstado);
 }
