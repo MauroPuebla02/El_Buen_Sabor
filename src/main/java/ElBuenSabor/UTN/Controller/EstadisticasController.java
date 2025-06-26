c353f611e8db4e77a52fd2d51607dd7c50fe61d3
MauroPuebla02 <mauronico02@gmail.com>
2025-06-25 21:29:06 -0300

    agrego Excel a estadisticas

--- src/main/java/ElBuenSabor/UTN/Controller/EstadisticasController.java
+++ src/main/java/ElBuenSabor/UTN/Controller/EstadisticasController.java
@@ -2,11 +2,16 @@ package ElBuenSabor.UTN.Controller;
 
 import ElBuenSabor.UTN.Models.ProjectionDTO.RendimientoChartProjectionDTOImpl;
 import ElBuenSabor.UTN.Service.Implements.EstadisticasServiceImpl;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 @RestController
@@ -32,5 +37,22 @@ public class EstadisticasController {
     @GetMapping("/clientes_ranking")
     public List<List<Object>> getRankingClientes(@RequestParam int periodo) {
         return estadisticasService.getRankingClientes(periodo);
+    }
+
+    @GetMapping("/exportar-excel")
+    public ResponseEntity<InputStreamResource> exportarExcel(@RequestParam int periodo) {
+        InputStreamResource file = new InputStreamResource(
+                estadisticasService.getExcelReporte(periodo)
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=Reporte_Estadisticas_" + periodo + ".xlsx");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.parseMediaType(
+                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                .body(file);
     }
 }
